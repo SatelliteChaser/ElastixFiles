0f5ac663a3d1446e2b71488d77f633765c12354c
SatelliteChaser <53260283+SatelliteChaser@users.noreply.github.com>
2020-10-31 14:08:51 +0100

    Add files via upload

---
+++ samplebatchfile.bat
@@ -0,0 +1,3 @@
+elastix -f "C:\path\to\L\image.png" -m "C:\path\to\R.png" -p param_rigid.txt -p param_nonrigid.txt -out proc\R
+elastix -f "C:\path\to\L\image.png" -m "C:\path\to\G.png" -p param_rigid.txt -p param_nonrigid.txt -out proc\G
+elastix -f "C:\path\to\L\image.png" -m "C:\path\to\B.png" -p param_rigid.txt -p param_nonrigid.txt -out proc\B
